ce0d5c33b9386bbdaf63fad3fae015024b3bb83b
AdoenkzHikki <37066436+AdoenkzHikki@users.noreply.github.com>
2018-05-09 09:45:36 +0700

    Add files via upload

---
+++ praktikum8-Method Get post/praktikum2.php
@@ -0,0 +1,10 @@
+<html>
+    <head> <title>Pengolahan Form 2</title> </head>
+    <body>
+        <form action="proses02.php" method="POST" name="input">
+            Nama anda : <input type="text" name="nama"><br>
+            <input type="submit" name="Input" value="Input">
+        </form>
+    </body>
+    </html>
+  
---
+++ praktikum8-Method Get post/prkatikum3.php
@@ -0,0 +1,10 @@
+<html>
+    <head> <title>Pengolahan Form 3</title> </head>
+    <body>
+        <form action="proses03.php" method="GET" name="frinput">
+            Nama anda : <input type="text" name="nama"><br>
+            <input type="submit" name="Input" value="Input">
+        </form>
+    </body>
+    </html>
+  
